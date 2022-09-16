091ef016fff2c12f273f06a7ac06884c49cfa1aa
Jie <jie@xiechenruideMacBook-Air.local>
2022-09-16 11:27:03 +0800

    add a basepage

---
+++ pratice/basepage.php
@@ -0,0 +1,8 @@
+<?php require __DIR__ . "/connect-db.php"?>
+<?php require __DIR__ . "/html-head.php";?>
+<?php require __DIR__ . "//navbar.php";?>
+<div class="container">
+     
+</div>
+<?php require __DIR__ . "/html-script.php";?>
+<?php require __DIR__ . "/html-footer.php";?>
--- pratice/navbar.php
+++ pratice/navbar.php
@@ -11,36 +11,38 @@
 
 <body>
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">列表</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">新增</a>
-                </li>
-            </ul>
-            <form class="form-inline my-2 my-lg-0">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">登入</a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">註冊</a>
-                </li>
-            </ul>
-            </form>
-        </div>
-    </nav>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">列表</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">新增</a>
+                    </li>
+                </ul>
+                <form class="form-inline my-2 my-lg-0">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">登入</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">註冊</a>
+                        </li>
+                    </ul>
+                </form>
+            </div>
+        </nav>
+    </div>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>

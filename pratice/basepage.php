e97f3166851d875cb72c9bd3b7fb5a6fba740e52
Jie <jie@xiechenruideMacBook-Air.local>
2022-09-16 11:50:07 +0800

    算出總頁數要有幾頁

--- pratice/basepage.php
+++ pratice/basepage.php
@@ -1,4 +1,27 @@
-<?php require __DIR__ . "/connect-db.php"?>
+<?php require __DIR__ . "/connect-db.php";
+
+// 每頁最多呈現幾筆資料
+$perpage = 20 ; 
+
+// 先來計算總筆數 做出分頁按鈕
+// 先寫 ＳＱＬ
+
+$t_sql = "SELECT COUNT(1) FROM address_book" ;
+
+// 把 $t_sql 這段帶進去資料庫拿資料
+
+$totalRow = $pdo->query($t_sql)->fetch(PDO::FETCH_NUM)[0];
+
+// 總頁數
+$totalPage = ceil($totalRow/$perpage);
+
+
+echo json_encode($totalPage);
+exit;
+
+?>
+
+
 <?php require __DIR__ . "/html-head.php";?>
 <?php require __DIR__ . "//navbar.php";?>
 <div class="container">
